fb5a20edb5a12428f1fd71bc8c87110540ee36d5
Liam <liam@mintigo.com>
2018-02-19 16:55:39 +0200

    working millliseconds

--- java101/Milliseconds/src/Milliseconds.java
+++ java101/Milliseconds/src/Milliseconds.java
@@ -8,45 +8,38 @@ public class Milliseconds {
         final int SEC_IN_HOUR = 3600;
         final int SEC_IN_DAY = 86400;
 
-        long d = 0;
-        long h = 0;
-        long m = 0;
-        long s = 0;
-
-        int t = 100 / SEC_IN_MINUTE;
-        System.out.println(t);
+        int d = 0;
+        int h = 0;
+        int m = 0;
+        int s = 0;
 
         Scanner scan = new Scanner(System.in);
         System.out.println("This program reads an integer which represents milliseconds and converts it to days," +
                 " hours, minuts and seconds.");
         System.out.println("Please enter the number of milliseconds: ");
-        // long ms = scan.nextLong();
-        long ms = 100000;
+        long ms = scan.nextLong();
 
-        long inSeconds = ms / 1000;
+        int inSeconds = (int) ms / 1000;
 
         while (inSeconds > 0) {
+
             if (inSeconds >= SEC_IN_DAY) {
-                d = ms / SEC_IN_DAY;
+                d = inSeconds / SEC_IN_DAY;
                 inSeconds = inSeconds - (d * SEC_IN_DAY);
-                System.out.println(ms);
 
             } else if (inSeconds >= SEC_IN_HOUR) {
-                h = ms / SEC_IN_HOUR;
+                h = inSeconds / SEC_IN_HOUR;
                 inSeconds = inSeconds - (h * SEC_IN_HOUR);
-                System.out.println(ms);
 
             } else if (inSeconds >= SEC_IN_MINUTE) {
-                m = ms / SEC_IN_MINUTE;
+                m = inSeconds / SEC_IN_MINUTE;
                 inSeconds = inSeconds - (m * SEC_IN_MINUTE);
-                System.out.println(ms);
+
             } else {
                 s = inSeconds;
                 inSeconds = 0;
             }
         }
         System.out.println("Day: " + d + " hour: " + h + " minutes: " + m + " seconds: " + s + "");
-        //System.out.println(inSeconds);
-
     } // end of main
 } // end of class milliseconds
